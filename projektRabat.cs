d38a01994a9880eb0f8a37a47a1f8ff3a1f554e1
Marcin Bernatowicz <bernatowicz.mar@gmail.com>
2023-09-10 17:55:23 +0200

    Aktualizacja - rabat

---
+++ projektRabat.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace projektRabat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj wartość zakupów: ");
+            int cena = int.Parse(Console.ReadLine());
+            if (cena > 400)
+            {
+                //cena -= 80;
+                cena = cena - 80;
+                Console.WriteLine("Rabat wynosi 80 zł");
+
+            }
+            else
+            {
+                if (cena > 200)
+                {
+                    cena -= 35;
+                    Console.WriteLine("Rabat wynosi 35 zł");
+                } else
+                {
+                    if (cena > 100)
+                    {
+                        cena -= 15;
+                        Console.WriteLine("Rabat wynosi 15 zł");
+                    }
+                    else 
+                    {
+
+                        Console.WriteLine("Brak rabatu");
+                    }
+
+                }
+            }
+            Console.WriteLine("Do zapłaty po rabacie: ");
+            Console.WriteLine(cena);
+            Console.Read();
+        }
+    }
+}
