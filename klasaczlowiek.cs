5749ac704449b6306abbdc3a24ff18fa31f6e9fd
Marcin Bernatowicz <bernatowicz.mar@gmail.com>
2024-01-15 21:22:50 +0100

    pierwsza klasa

---
+++ klasaczlowiek.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LekcjaKO
+{
+    class Czlowiek
+    {
+        public Czlowiek()
+        {
+            
+        }
+
+        public Czlowiek(string imieKonstruktora) 
+        {
+            imie = imieKonstruktora;
+        }
+        
+        public Czlowiek(string imieKonstruktora, string nazwiskokons)
+        {
+            imie = imieKonstruktora;
+            nazwisko = nazwiskokons;
+        }
+        public string imie = "BRAK";
+        public string nazwisko  = "BRAK";
+
+        public void PrzedstawSie()
+        {
+            Console.WriteLine("Mam na imię : {0} , a na nazwisko : {1} " + imie,nazwisko);
+        }
+    }
+}
---
+++ programczlowiek.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LekcjaKO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj, za chwile utworzymy obiekt w klasie 'Człowiek' !");
+           
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Podaj imię pierwszego obiektu : ");
+            string name = Console.ReadLine();
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Podaj nazwisko pierwszego obiektu : ");
+            string surname = Console.ReadLine();
+
+            Czlowiek pierwszy = new Czlowiek(name,surname);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            pierwszy.PrzedstawSie();
+            
+            
+            Console.ReadKey();
+        }
+    }
+    
+
+}
